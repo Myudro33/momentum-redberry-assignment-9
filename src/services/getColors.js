cd722ad70ba36985c5f65d2aff1554f224ac9311
NikaQanashvili <qanashvilinika7@gmail.com>
2025-03-12 22:55:56 +0400

    style: get colors by priority

--- src/services/getColors.js
+++ src/services/getColors.js
@@ -1,7 +1,16 @@
-const getColor = () => {
+const getStatusBackgroundColor = () => {
     const root = document.documentElement;
-    const colors = [getComputedStyle(root).getPropertyValue('--pink'),getComputedStyle(root).getPropertyValue('--blue'),getComputedStyle(root).getPropertyValue('--orange'),getComputedStyle(root).getPropertyValue('--yellow')]
+    const colors = [getComputedStyle(root).getPropertyValue('--yellow'),getComputedStyle(root).getPropertyValue('--orange'),getComputedStyle(root).getPropertyValue('--pink'),getComputedStyle(root).getPropertyValue('--blue')]
    return colors
 }
+const getPriorityBackgroundColor = (id)=>{
+    if(id===1){
+        return '#08A508'
+    }else if(id===2){
+        return '#FFBE0B'
+    }else{
+        return '#FA4D4D'
+    }
+}
 
-export default getColor
+export {getStatusBackgroundColor,getPriorityBackgroundColor}
