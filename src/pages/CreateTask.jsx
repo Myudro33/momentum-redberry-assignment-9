6813ab8a3b90ee08b5afb86fe24bd80297db4167
NikaQanashvili <qanashvilinika7@gmail.com>
2025-03-15 00:24:58 +0400

    refactor: make api calls with promise all

--- src/pages/CreateTask.jsx
+++ src/pages/CreateTask.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Formik, Form } from "formik";
 import TheInput from "../components/TheInput";
 import { taskInitialSchema, taksValidation } from "../utils/formValidations";
@@ -6,7 +6,36 @@ import TheTextarea from "../components/TheTextarea";
 import TheSelect from "../components/TheSelect";
 import TheDatePicker from "../components/TheDatePicker";
 import TheButton from "../components/TheButton";
+import axios from "../services/axiosService";
+
 const CreateTask = () => {
+  const [priorities, setPriorities] = useState([]);
+  const [statuses, setStatuses] = useState([]);
+  const [departments, setDepartments] = useState([]);
+  const [employees, setEmployees] = useState([]);
+  const [filteredEmployees, setFilteredEmployees] = useState([]);
+  const [selectedValue, setSelectedValue] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [priorities, statuses, departments, employees] = await Promise.all([
+          axios({method: 'GET', endpoint: "/priorities"}),
+          axios({method: 'GET', endpoint: "/statuses"}),
+          axios({method: 'GET', endpoint: "/departments"}),
+          axios({method: 'GET', endpoint: "/employees"}),
+        ]);
+        setPriorities(priorities);
+        setStatuses(statuses);
+        setDepartments(departments);
+        setEmployees(employees);
+      } catch (error) {
+        console.error("Failed to fetch data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="px-[7.5rem] mt-5">
       <h1 className="text-2xl font-semibold">შექმენი ახალი დავალება</h1>
@@ -14,7 +43,7 @@ const CreateTask = () => {
         initialValues={taskInitialSchema}
         validationSchema={taksValidation}
         onSubmit={(values) => {
-          alert(JSON.stringify(values));
+          axios({method: 'POST', endpoint: "/tasks", body: values});
         }}
       >
         {({
@@ -26,11 +55,11 @@ const CreateTask = () => {
           handleChange,
         }) => (
           <Form
-            className="w-full flex flex-col items-end p-10  mt-5"
+            className="w-full flex flex-col items-end p-10 mt-5"
             onSubmit={handleSubmit}
           >
             <div className="w-full flex justify-between">
-              <div className="w-[43%] h-[25rem] flex flex-col justify-between ">
+              <div className="w-[43%] h-[25rem] flex flex-col justify-between">
                 <TheInput
                   name="name"
                   onChange={handleChange}
@@ -47,27 +76,37 @@ const CreateTask = () => {
                   label="აღწერა"
                   width="100%"
                 />
-                <div className="flex w-full justify-between mt-28 ">
-                  <TheSelect
-                    name="priority_id"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.priority_id}
-                    label="პრიორიტეტი"
-                    width="47%"
-                  />
-                  <TheSelect
-                    name="status_id"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.status_id}
-                    label="სტატუსი"
-                    width="47%"
-                  />
+                <div className="flex w-full justify-between mt-28">
+                  {priorities?.length > 0 && (
+                    <TheSelect
+                      name="priority_id"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.priority_id}
+                      label="პრიორიტეტი"
+                      width="47%"
+                      data={priorities}
+                      setFieldValue={setFieldValue}
+                      defaultValue={priorities[1]}
+                    />
+                  )}
+                  {statuses?.length > 0 && (
+                    <TheSelect
+                      name="status_id"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.status_id}
+                      label="სტატუსი"
+                      width="47%"
+                      data={statuses}
+                      setFieldValue={setFieldValue}
+                      defaultValue={statuses[0]}
+                    />
+                  )}
                 </div>
               </div>
-              <div className="w-[43%] h-[25rem]  flex flex-col justify-between">
-                <div className="h-[12rem] flex flex-col justify-between ">
+              <div className="w-[43%] h-[23.6rem] flex flex-col justify-between">
+                <div className="h-[12.5rem] flex flex-col justify-between">
                   <TheSelect
                     name="department_id"
                     onChange={handleChange}
@@ -75,6 +114,12 @@ const CreateTask = () => {
                     value={values.department_id}
                     label="დეპარტამენტი"
                     width="100%"
+                    data={departments}
+                    setFieldValue={setFieldValue}
+                    employees={employees}
+                    filteredEmployees={filteredEmployees}
+                    setFilteredEmployees={setFilteredEmployees}
+                    setSelectedValue={setSelectedValue}
                   />
                   <TheSelect
                     name="employee_id"
@@ -82,7 +127,11 @@ const CreateTask = () => {
                     onBlur={handleBlur}
                     value={values.employee_id}
                     label="პასუხისმგებელი თანამშრომელი"
+                    selectedValue={selectedValue}
                     width="100%"
+                    data={filteredEmployees}
+                    setFieldValue={setFieldValue}
+                    setSelectedValue={setSelectedValue}
                   />
                 </div>
                 <TheDatePicker
@@ -90,7 +139,7 @@ const CreateTask = () => {
                   onChange={handleChange}
                   onBlur={handleBlur}
                   value={values.due_date}
-                  label="დეპარტამენტი"
+                  label="დედლაინი"
                   width="50%"
                 />
               </div>
