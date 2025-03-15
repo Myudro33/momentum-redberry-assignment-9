4534a65d3840664f6df38c3896832afa5e49992a
NikaQanashvili <qanashvilinika7@gmail.com>
2025-03-15 11:07:16 +0400

    chore: disable resize to textarea

--- src/components/TheTextarea.jsx
+++ src/components/TheTextarea.jsx
@@ -10,7 +10,7 @@ const TheTextarea = (props) => {
             onChange={props.onChange}
             onBlur={props.onBlur}
             value={props.value}
-            className="p-2 h-[8.313rem] shrink-0 rounded-md border border-[color:var(--border-color)] outline-none "
+            className="p-2 h-[8.313rem] resize-none shrink-0 rounded-md border border-[color:var(--border-color)] outline-none "
             type="text"
           />
           <span className="flex items-center h-5 shrink-0">
