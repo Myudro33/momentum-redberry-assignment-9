90a5a0b3a20cb5f42788c73e8574aa5d91140ca8
NikaQanashvili <qanashvilinika7@gmail.com>
2025-03-15 18:42:29 +0400

    feat: add textarea component

---
+++ src/components/CommentTextarea.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import TheButton from "../components/TheButton";
+import axios from "../services/axiosService";
+
+const CommentTextarea = ({
+  id,
+  parent,
+  comments,
+  setComments,
+  parent_id,
+  style,
+  setTextarea,
+}) => {
+  const [value, setValue] = useState("");
+  const [error, setError] = useState();
+  const click = async () => {
+    try {
+      if (!value.trim()) {
+        setError("სავალდებულო");
+        return;
+      }
+      setError('')
+      const newComment = await axios({
+        endpoint: `/tasks/${id}/comments`,
+        method: "POST",
+        body: parent_id ? { text: value, parent_id } : { text: value },
+      });
+      if (parent) {
+        setComments([newComment, ...comments]);
+      } else {
+        setComments((comments) =>
+          comments.map((comment) =>
+            comment.id === newComment.parent_id
+              ? {
+                  ...comment,
+                  sub_comments: [...(comment.sub_comments || []), newComment]
+                }
+              : comment
+          )
+        );
+        setTextarea(false);
+      }
+      setValue("");
+    } catch (error) {
+      alert(error);
+    }
+  };
+  return (
+    <>
+      <div style={style} className="relative w-full h-32">
+        <textarea
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          className="resize-none w-full h-full p-2"
+        />
+        <TheButton
+          onClick={click}
+          style={{ position: "absolute", bottom: 10, right: 10 }}
+          type="submit"
+          rounded
+          solid
+          text="დააკომენტარე"
+        />
+      </div>
+      {error && <p className="text-red-500">{error}</p>}
+    </>
+  );
+};
+
+export default CommentTextarea;
