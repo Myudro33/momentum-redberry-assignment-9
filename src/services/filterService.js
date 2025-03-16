34caf37cd7d008f9cf12cd8f4f922a676488b2cf
NikaQanashvili <qanashvilinika7@gmail.com>
2025-03-16 19:45:46 +0400

    feat: add filter service functions

---
+++ src/services/filterService.js
@@ -0,0 +1,39 @@
+  const applyFilters = (selectedDepartments, selectedEmployees, selectedPriorities, setFilteredData, tasks) => {
+    let filtered = [...tasks];
+  
+    if (selectedDepartments.length) {
+      filtered = filtered.filter((task) =>
+        selectedDepartments.includes(task.department?.name)
+      );
+    }
+
+    if (selectedPriorities.length) {
+      filtered = filtered.filter((task) =>
+        selectedPriorities.includes(task.priority?.name)
+      );
+    }
+  
+    if (selectedEmployees.length) {
+      filtered = filtered.filter((task) =>
+        selectedEmployees.includes(task.employee.id)
+      );
+    }
+  
+    setFilteredData(filtered.length > 0 ? filtered : []);
+  };
+  
+  const setQueryParams = (setSearchParams, filters, searchParams) => {
+    const queryParams = new URLSearchParams(searchParams);
+  
+    Object.entries(filters).forEach(([key, value]) => {
+      if (value.length) {
+        queryParams.set(key, value.join(","));
+      } else {
+        queryParams.delete(key);
+      }
+    });
+  
+    setSearchParams(queryParams);
+  };
+
+  export {applyFilters,setQueryParams}
