dd6e457bf4840eed954856897ed95cc46b92c67d
NikaQanashvili <qanashvilinika7@gmail.com>
2025-03-18 17:36:39 +0400

    feat: create localstorage service

---
+++ src/services/useLocalStorage.js
@@ -0,0 +1,11 @@
+export const useLocalStorage = () =>{
+    const setItem = (key,value)=>{
+        window.localStorage.setItem(key,JSON.stringify(value))
+    }
+
+    const getItem = (key)=>{
+       return JSON.parse(window.localStorage.getItem(key))
+    }
+
+    return {setItem,getItem}
+}
