8d80df25caac23faa59e90fce0bd7c88ce004a3f
NikaQanashvili <qanashvilinika7@gmail.com>
2025-03-18 13:57:51 +0400

    fix: make select input drop on click instead hover

--- src/components/TheSelect.jsx
+++ src/components/TheSelect.jsx
@@ -4,6 +4,8 @@ import EmployeeCard from "./EmployeeCard";
 import plusIcon from "../assets/plus-icon.png";
 import axios from "../services/axiosService";
 import checkRed from "../assets/check-red.png";
+import solidArrow from '../assets/arrow-down-solid.png'
+import arrowDown from '../assets/arrow-down.png'
 
 const TheSelect = ({
   name,
@@ -22,6 +24,7 @@ const TheSelect = ({
   touched,
 }) => {
   const [selected, setSelected] = useState();
+  const [dropdown, setDropdown] = useState(false);
 
   const handleChange = (item) => {
     if (name === "status") {
@@ -76,36 +79,42 @@ const TheSelect = ({
     <div style={{ width }} className="h-25 flex flex-col">
       <label className="font-semibold text-sm">{label} *</label>
       <div
+        onClick={() => setDropdown((prev) => !prev)}
         className="relative inline-block dropdown h-11 w-full p-2 shrink-0 rounded-md border"
         style={{ borderColor }}
       >
-        <button type="button" className="flex items-center text-sm h-8 w-full">
+        <button type="button" className="flex items-center text-sm h-8 w-full relative">
           {selected?.icon && (
             <img className="mr-2" src={selected.icon} alt="icon" />
           )}
           {name === "employee_id"
             ? renderEmployeeCard(selectedValue)
             : selected?.name}
+            <img className={`absolute right-0 ${!dropdown&&'rotate-180'} transition-all `} src={dropdown?solidArrow:arrowDown} alt="arrow" />
         </button>
-        <ul className="dropdown-menu w-full left-0 mt-5 top-6">
-          {name === "employee_id" && (
-            <button
-              className="text-[color:var(--solid-button)] cursor-pointer flex items-center w-full p-2"
-              type="button"
-              onClick={() => setModal("employee")}
-            >
-              <img className="mr-2" src={plusIcon} alt="icon" /> დაამატე
-              თანამშრომელი
-            </button>
-          )}
-          {data &&
-            data.map((item) => (
-              <li key={item.id} onClick={() => handleChange(item)}>
-                {item.icon && <img src={item.icon} alt="icon" />}
-                {name === "employee_id" ? renderEmployeeCard(item) : item.name}
-              </li>
-            ))}
-        </ul>
+        {dropdown && (
+          <ul className="w-full left-0 mt-5 top-6 absolute list-none bg-white border border-[var(--solid-button)] rounded-md z-10 p-2">
+            {name === "employee_id" && (
+              <button
+                className="text-[color:var(--solid-button)] cursor-pointer flex items-center w-full p-2"
+                type="button"
+                onClick={() => setModal("employee")}
+              >
+                <img className="mr-2" src={plusIcon} alt="icon" /> დაამატე
+                თანამშრომელი
+              </button>
+            )}
+            {data &&
+              data.map((item) => (
+                <li className="flex items-center hover:bg-blue-50 p-1 cursor-pointer" key={item.id} onClick={() => handleChange(item)}>
+                  {item.icon && <img src={item.icon} alt="icon" />}
+                  {name === "employee_id"
+                    ? renderEmployeeCard(item)
+                    : item.name}
+                </li>
+              ))}
+          </ul>
+        )}
       </div>
       {hasError && (
         <p className="text-red-500 flex items-center">
