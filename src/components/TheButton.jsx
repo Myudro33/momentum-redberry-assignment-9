cedc4323f71fbf5d3a3219240947dc66a9907ab3
NikaQanashvili <qanashvilinika7@gmail.com>
2025-03-18 19:55:17 +0400

    refactor: destructure props in TheButton and TheTaskCard components for improved readability

--- src/components/TheButton.jsx
+++ src/components/TheButton.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const TheButton = (props) => {
+const TheButton = ({ onClick, style, type, rounded, solid, text }) => {
   return (
-    <button style={props.style} type={props.type} onClick={props.onClick} className={`py-2.5 px-5  cursor-pointer   ${props.solid?'bg-[color:var(--purple)] hover:bg-[color:var(--light-purple)]  text-white':"border border-[color:var(--purple)] hover:border-[color:var(--light-purple)] "} ${props.rounded?'rounded-[1.3rem]':'rounded-[.3rem]'} `} >
-      {props.text}
+    <button
+      style={style}
+      type={type}
+      onClick={onClick}
+      className={`py-2.5 px-5  cursor-pointer   ${
+        solid
+          ? "bg-[color:var(--purple)] hover:bg-[color:var(--light-purple)]  text-white"
+          : "border border-[color:var(--purple)] hover:border-[color:var(--light-purple)] "
+      } ${rounded ? "rounded-[1.3rem]" : "rounded-[.3rem]"} `}
+    >
+      {text}
     </button>
-  )
-}
+  );
+};
 
-export default TheButton
+export default TheButton;
--- src/components/TheTaskCard.jsx
+++ src/components/TheTaskCard.jsx
@@ -8,53 +8,53 @@ import {DateFormatter} from "../services/dateFormater";
 import comment from "../assets/comment-icon.png";
 import { Link } from "react-router-dom";
 
-const TheTaskCard = (props) => {
+const TheTaskCard = ({item,index}) => {
     
   return (
-    <Link to={`/task/${props.item.id}`} >
+    <Link to={`/task/${item.id}`} >
     <div
-      key={props.item.id}
-      style={{ borderColor: getStatusBackgroundColor()[props.index] }}
+      key={item.id}
+      style={{ borderColor: getStatusBackgroundColor()[index] }}
       className="w-full my-6 rounded-[.938rem] h-[13.563rem] p-5 border "
     >
       <div className="flex items-center justify-between">
         <div className="flex">
           <div
             style={{
-              borderColor: getPriorityBackgroundColor(props.item.priority.id),
-              color: getPriorityBackgroundColor(props.item.priority.id),
+              borderColor: getPriorityBackgroundColor(item.priority.id),
+              color: getPriorityBackgroundColor(item.priority.id),
             }}
             className="w-[5.9rem] h-[1.625rem] flex justify-center items-center border p-1 rounded-[.25rem]"
           >
-            <img src={props.item.priority.icon} alt="priority icon" />
-            <p className="text-xs">{props.item.priority.name}</p>
+            <img src={item.priority.icon} alt="priority icon" />
+            <p className="text-xs">{item.priority.name}</p>
           </div>
           <p
             className="px-2 text-xs  h-6 text-white flex justify-center items-center rounded-2xl ml-2"
             style={{
-              backgroundColor: getDepartmentInfo(props.item.department.id).color,
+              backgroundColor: getDepartmentInfo(item.department.id).color,
             }}
           >
-            {getDepartmentInfo(props.item.department.id).name}
+            {getDepartmentInfo(item.department.id).name}
           </p>
         </div>
-        <p className="text-xs">{DateFormatter(props.item.due_date)}</p>
+        <p className="text-xs">{DateFormatter(item.due_date)}</p>
       </div>
       <div className="mx-3 mt-5">
-        <h1 className="font-bold">{props.item.name}</h1>
+        <h1 className="font-bold">{item.name}</h1>
         <p className="text-[#343A40] h-8 text-xs mt-4 overflow-y-clip">
-          {props.item.description}
+          {item.description}
         </p>
       </div>
       <div className="flex justify-between items-center w-full mt-6">
         <img
           className="w-8 h-8 rounded-full"
-          src={props.item.employee.avatar}
+          src={item.employee.avatar}
           alt="avatar"
         />
         <div className="flex items-center">
           <img src={comment} alt="icon" />
-          <p className="ml-1">{props.item.total_comments}</p>
+          <p className="ml-1">{item.total_comments}</p>
         </div>
       </div>
     </div>
