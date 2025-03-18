40bbc9908584d94993d1c1e250b8673e79f54a0f
NikaQanashvili <qanashvilinika7@gmail.com>
2025-03-18 20:38:50 +0400

    feat: set default tomorrow date for datepicker

--- src/components/TheDatePicker.jsx
+++ src/components/TheDatePicker.jsx
@@ -1,21 +1,29 @@
 import { ErrorMessage } from "formik";
-import React,{useEffect} from "react";
+import React,{useEffect,useState} from "react";
 import checkRed from "../assets/check-red.png";
 import { useLocalStorage } from "../services/useLocalStorage";
 
-const TheDatePicker = ({ field, form: { errors, touched }, ...props }) => {
+const TheDatePicker = ({ field, form: { errors, touched, setFieldValue }, ...props }) => {
+  const [date, setDate] = useState("");
   const hasError = errors[field.name] && touched[field.name];
   const borderColor = hasError
     ? "red"
     : touched[field.name]
     ? "green"
     : "var(--gray-border)";
-  const { setItem } = useLocalStorage();
+  const { setItem,getItem } = useLocalStorage();
   useEffect(() => {
-    if (props.store) {
       setItem(field.name, field.value);
-    }
   }, [field]);
+  useEffect(() => {
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    setDate(tomorrow.toISOString().split("T")[0]);
+    if(!getItem('due_date')){
+      setFieldValue(field.name,  tomorrow.toISOString().split("T")[0]); 
+      setItem(field.name, tomorrow.toISOString().split("T")[0]);
+    } 
+  }, []);
   return (
     <div
       style={{ width: props.width || "22rem" }}
@@ -24,9 +32,15 @@ const TheDatePicker = ({ field, form: { errors, touched }, ...props }) => {
       <label className="font-semibold text-sm">{props.label} *</label>
       <input
         {...field}
+        value={getItem('due_date') || date} 
         type="date"
         className="h-11 p-2 shrink-0 rounded-md border outline-none"
         style={{ borderColor }}
+        onChange={(e) => {
+          const value = e.target.value;
+          setFieldValue(field.name, value); 
+          setDate(value);
+        }}
       />
       {hasError && (
         <p className="text-red-500 flex items-center">
